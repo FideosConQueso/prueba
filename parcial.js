16bbea3c391de8787b4264b6d7db3018d4690ae6
Fede <federicorivero002@gmail.com>
2024-10-08 19:29:48 -0300

    agregar archivo

---
+++ parcial.js
@@ -0,0 +1,37 @@
+
+// ejercicio 2
+
+
+function calcular(){
+    n1 = Number(prompt ("pone un numer --> "));
+    n2 = Number(prompt("pone un numer --> "));
+    n3 = Number(prompt("pone un numer --> "));
+
+    resta = n1 - n2 - n3;
+    multi = n1 / n2 / n3;
+    if (resta > 30 && div < 400){
+        console.log("Se cumplen");
+    }else {
+        console.log("No se cumple");
+    }
+}
+
+
+calcular();
+
+
+//ejercicio3
+/*
+function entreNumeros (){
+    n1 = Number(prompt ("Pone un numer --> "));
+    n2 = Number(prompt("pone un numer --> "));
+
+    for ( let index = n1; index <= n2; index++){
+        if ( index & 2 != 0) {
+            console.log(index);
+        }
+    }
+}
+
+// entreNumeros();
+*/
